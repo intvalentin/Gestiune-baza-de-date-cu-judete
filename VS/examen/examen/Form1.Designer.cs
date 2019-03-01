144ec003299e70412ee42c79d3481622c1cbbe68
intvalentin <47526377+intvalentin@users.noreply.github.com>
2019-03-01 14:26:15 +0200

    first

---
+++ VS/examen/examen/Form1.Designer.cs
@@ -0,0 +1,352 @@
+﻿namespace examen
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.dLocalitateDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.codLocalitateDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.DJudet = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.dataTable1BindingSource = new System.Windows.Forms.BindingSource(this.components);
+            this.dataSet1 = new examen.DataSet1();
+            this.localitatiBindingSource = new System.Windows.Forms.BindingSource(this.components);
+            this.btnAdaugare = new System.Windows.Forms.Button();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.PB = new System.Windows.Forms.PictureBox();
+            this.txtIDL = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.txtSPI = new System.Windows.Forms.TextBox();
+            this.txtCL = new System.Windows.Forms.TextBox();
+            this.txtDL = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.cmbJudet = new System.Windows.Forms.ComboBox();
+            this.judeteBindingSource = new System.Windows.Forms.BindingSource(this.components);
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.btnConfirmare = new System.Windows.Forms.Button();
+            this.btnRenuntare = new System.Windows.Forms.Button();
+            this.judeteTableAdapter = new examen.DataSet1TableAdapters.JudeteTableAdapter();
+            this.localitatiTableAdapter = new examen.DataSet1TableAdapters.LocalitatiTableAdapter();
+            this.dataTable1TableAdapter = new examen.DataSet1TableAdapters.DataTable1TableAdapter();
+            this.lblOp = new System.Windows.Forms.Label();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataTable1BindingSource)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataSet1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.localitatiBindingSource)).BeginInit();
+            this.groupBox1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.PB)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.judeteBindingSource)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AllowUserToAddRows = false;
+            this.dataGridView1.AllowUserToDeleteRows = false;
+            this.dataGridView1.AutoGenerateColumns = false;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.dLocalitateDataGridViewTextBoxColumn,
+            this.codLocalitateDataGridViewTextBoxColumn,
+            this.DJudet});
+            this.dataGridView1.DataSource = this.dataTable1BindingSource;
+            this.dataGridView1.Location = new System.Drawing.Point(12, 141);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.ReadOnly = true;
+            this.dataGridView1.Size = new System.Drawing.Size(347, 268);
+            this.dataGridView1.TabIndex = 0;
+            // 
+            // dLocalitateDataGridViewTextBoxColumn
+            // 
+            this.dLocalitateDataGridViewTextBoxColumn.DataPropertyName = "DLocalitate";
+            this.dLocalitateDataGridViewTextBoxColumn.HeaderText = "DLocalitate";
+            this.dLocalitateDataGridViewTextBoxColumn.Name = "dLocalitateDataGridViewTextBoxColumn";
+            this.dLocalitateDataGridViewTextBoxColumn.ReadOnly = true;
+            // 
+            // codLocalitateDataGridViewTextBoxColumn
+            // 
+            this.codLocalitateDataGridViewTextBoxColumn.DataPropertyName = "CodLocalitate";
+            this.codLocalitateDataGridViewTextBoxColumn.HeaderText = "CodLocalitate";
+            this.codLocalitateDataGridViewTextBoxColumn.Name = "codLocalitateDataGridViewTextBoxColumn";
+            this.codLocalitateDataGridViewTextBoxColumn.ReadOnly = true;
+            // 
+            // DJudet
+            // 
+            this.DJudet.DataPropertyName = "DJudet";
+            this.DJudet.HeaderText = "DJudet";
+            this.DJudet.Name = "DJudet";
+            this.DJudet.ReadOnly = true;
+            // 
+            // dataTable1BindingSource
+            // 
+            this.dataTable1BindingSource.DataMember = "DataTable1";
+            this.dataTable1BindingSource.DataSource = this.dataSet1;
+            // 
+            // dataSet1
+            // 
+            this.dataSet1.DataSetName = "DataSet1";
+            this.dataSet1.SchemaSerializationMode = System.Data.SchemaSerializationMode.IncludeSchema;
+            // 
+            // localitatiBindingSource
+            // 
+            this.localitatiBindingSource.DataMember = "Localitati";
+            this.localitatiBindingSource.DataSource = this.dataSet1;
+            // 
+            // btnAdaugare
+            // 
+            this.btnAdaugare.Location = new System.Drawing.Point(381, 73);
+            this.btnAdaugare.Name = "btnAdaugare";
+            this.btnAdaugare.Size = new System.Drawing.Size(75, 23);
+            this.btnAdaugare.TabIndex = 1;
+            this.btnAdaugare.Text = "Adaugare";
+            this.btnAdaugare.UseVisualStyleBackColor = true;
+            this.btnAdaugare.Click += new System.EventHandler(this.btnAdaugare_Click);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.PB);
+            this.groupBox1.Controls.Add(this.txtIDL);
+            this.groupBox1.Controls.Add(this.label5);
+            this.groupBox1.Controls.Add(this.txtSPI);
+            this.groupBox1.Controls.Add(this.txtCL);
+            this.groupBox1.Controls.Add(this.txtDL);
+            this.groupBox1.Controls.Add(this.label4);
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Controls.Add(this.cmbJudet);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Location = new System.Drawing.Point(381, 141);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(563, 281);
+            this.groupBox1.TabIndex = 2;
+            this.groupBox1.TabStop = false;
+            // 
+            // PB
+            // 
+            this.PB.Location = new System.Drawing.Point(318, 56);
+            this.PB.Name = "PB";
+            this.PB.Size = new System.Drawing.Size(239, 203);
+            this.PB.SizeMode = System.Windows.Forms.PictureBoxSizeMode.CenterImage;
+            this.PB.TabIndex = 10;
+            this.PB.TabStop = false;
+            // 
+            // txtIDL
+            // 
+            this.txtIDL.Location = new System.Drawing.Point(149, 196);
+            this.txtIDL.Name = "txtIDL";
+            this.txtIDL.Size = new System.Drawing.Size(46, 20);
+            this.txtIDL.TabIndex = 9;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(21, 203);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(68, 13);
+            this.label5.TabIndex = 8;
+            this.label5.Text = "Id Localitate:";
+            // 
+            // txtSPI
+            // 
+            this.txtSPI.Location = new System.Drawing.Point(149, 154);
+            this.txtSPI.Name = "txtSPI";
+            this.txtSPI.Size = new System.Drawing.Size(151, 20);
+            this.txtSPI.TabIndex = 7;
+            this.txtSPI.DoubleClick += new System.EventHandler(this.txtSPI_DoubleClick);
+            // 
+            // txtCL
+            // 
+            this.txtCL.Location = new System.Drawing.Point(149, 69);
+            this.txtCL.Name = "txtCL";
+            this.txtCL.Size = new System.Drawing.Size(151, 20);
+            this.txtCL.TabIndex = 6;
+            this.txtCL.Leave += new System.EventHandler(this.txtCL_Leave);
+            // 
+            // txtDL
+            // 
+            this.txtDL.Location = new System.Drawing.Point(149, 33);
+            this.txtDL.Name = "txtDL";
+            this.txtDL.Size = new System.Drawing.Size(148, 20);
+            this.txtDL.TabIndex = 5;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(21, 161);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(47, 13);
+            this.label4.TabIndex = 4;
+            this.label4.Text = "Imagine:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(21, 115);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(36, 13);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "Judet:";
+            // 
+            // cmbJudet
+            // 
+            this.cmbJudet.FormattingEnabled = true;
+            this.cmbJudet.Location = new System.Drawing.Point(149, 107);
+            this.cmbJudet.Name = "cmbJudet";
+            this.cmbJudet.Size = new System.Drawing.Size(151, 21);
+            this.cmbJudet.TabIndex = 2;
+            this.cmbJudet.SelectedIndexChanged += new System.EventHandler(this.cmbJudet_SelectedIndexChanged);
+            // 
+            // judeteBindingSource
+            // 
+            this.judeteBindingSource.DataMember = "Judete";
+            this.judeteBindingSource.DataSource = this.dataSet1;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(18, 76);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(78, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Cod Localitate:";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(18, 33);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(104, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Denumire Localitate:";
+            // 
+            // btnConfirmare
+            // 
+            this.btnConfirmare.Location = new System.Drawing.Point(482, 73);
+            this.btnConfirmare.Name = "btnConfirmare";
+            this.btnConfirmare.Size = new System.Drawing.Size(75, 23);
+            this.btnConfirmare.TabIndex = 3;
+            this.btnConfirmare.Text = "Confirmare";
+            this.btnConfirmare.UseVisualStyleBackColor = true;
+            this.btnConfirmare.Click += new System.EventHandler(this.btnConfirmare_Click);
+            // 
+            // btnRenuntare
+            // 
+            this.btnRenuntare.Location = new System.Drawing.Point(595, 73);
+            this.btnRenuntare.Name = "btnRenuntare";
+            this.btnRenuntare.Size = new System.Drawing.Size(75, 23);
+            this.btnRenuntare.TabIndex = 4;
+            this.btnRenuntare.Text = "Renuntare";
+            this.btnRenuntare.UseVisualStyleBackColor = true;
+            this.btnRenuntare.Click += new System.EventHandler(this.btnRenuntare_Click);
+            // 
+            // judeteTableAdapter
+            // 
+            this.judeteTableAdapter.ClearBeforeFill = true;
+            // 
+            // localitatiTableAdapter
+            // 
+            this.localitatiTableAdapter.ClearBeforeFill = true;
+            // 
+            // dataTable1TableAdapter
+            // 
+            this.dataTable1TableAdapter.ClearBeforeFill = true;
+            // 
+            // lblOp
+            // 
+            this.lblOp.AutoSize = true;
+            this.lblOp.Location = new System.Drawing.Point(381, 122);
+            this.lblOp.Name = "lblOp";
+            this.lblOp.Size = new System.Drawing.Size(0, 13);
+            this.lblOp.TabIndex = 5;
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(954, 454);
+            this.Controls.Add(this.lblOp);
+            this.Controls.Add(this.btnRenuntare);
+            this.Controls.Add(this.btnConfirmare);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.btnAdaugare);
+            this.Controls.Add(this.dataGridView1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataTable1BindingSource)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataSet1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.localitatiBindingSource)).EndInit();
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.PB)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.judeteBindingSource)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Button btnAdaugare;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.TextBox txtSPI;
+        private System.Windows.Forms.TextBox txtCL;
+        private System.Windows.Forms.TextBox txtDL;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.ComboBox cmbJudet;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btnConfirmare;
+        private System.Windows.Forms.Button btnRenuntare;
+        private DataSet1 dataSet1;
+        private System.Windows.Forms.BindingSource judeteBindingSource;
+        private DataSet1TableAdapters.JudeteTableAdapter judeteTableAdapter;
+        private System.Windows.Forms.BindingSource localitatiBindingSource;
+        private DataSet1TableAdapters.LocalitatiTableAdapter localitatiTableAdapter;
+        private System.Windows.Forms.PictureBox PB;
+        private System.Windows.Forms.TextBox txtIDL;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.BindingSource dataTable1BindingSource;
+        private DataSet1TableAdapters.DataTable1TableAdapter dataTable1TableAdapter;
+        private System.Windows.Forms.DataGridViewTextBoxColumn dLocalitateDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn codLocalitateDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn DJudet;
+        private System.Windows.Forms.Label lblOp;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+    }
+}
+
---
+++ VS/examen/examen/Form1.cs
@@ -0,0 +1,296 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Data.OleDb;
+
+namespace examen
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            
+            A1();
+
+        }
+
+        private void btnAdaugare_Click(object sender, EventArgs e)
+        {
+            A2();
+        }
+
+        private void A1()
+        {
+            dataTable1TableAdapter.Fill(dataSet1.DataTable1);
+            
+
+            cmbJudet.DropDownStyle = ComboBoxStyle.Simple;
+
+            PB.SizeMode = PictureBoxSizeMode.StretchImage;
+
+            dataGridView1.AllowUserToAddRows = false;
+            dataGridView1.AllowUserToDeleteRows = false;
+            dataGridView1.ReadOnly = true;
+
+            txtIDL.ReadOnly = true;
+
+            A3();
+        }
+
+        private void A2()
+        {
+            //Configurare butoane
+            configureazaButoane(false);
+            //Dezlegare controale Panel
+            legareControale(false);
+            //Ridicare protectie controale Panel
+            protectiePanel(false);
+            //Configurare(ComboBox - Uri)
+            cmbJudet.DropDownStyle = ComboBoxStyle.DropDownList;
+            //Legare comboBox-uri la sursa de date
+            cmbJudet.DataSource = dataTable1BindingSource;
+            cmbJudet.DisplayMember = "DJudet";
+            cmbJudet.ValueMember = "Judete_IdJudet";
+            
+            //Modifcare lblOp
+            lblOp.Text = "ADAUGARE";
+            //Pozitionare cursor pe primul camp
+            txtDL.Focus();
+            // Golire campuri
+            golireCampuri();
+        }
+
+
+        private void configureazaButoane(bool v)
+        {
+            btnRenuntare.Visible = !v;
+            btnConfirmare.Visible = !v;
+            btnAdaugare.Enabled = v;
+            
+        }
+
+        private void legareControale(bool v)
+        {
+            if (v)
+            {
+                txtDL.DataBindings.Add("Text", dataTable1BindingSource, "DLocalitate");
+                txtCL.DataBindings.Add("Text", dataTable1BindingSource, "CodLocalitate");
+
+                cmbJudet.DataBindings.Add("Text", dataTable1BindingSource, "DJudet");
+                txtSPI.DataBindings.Add("Text", dataTable1BindingSource, "SpImagine");
+                txtIDL.DataBindings.Add("Text", dataTable1BindingSource, "IdLocalitate");
+                PB.DataBindings.Add("ImageLocation", dataTable1BindingSource, "SpImagine");
+            }
+            else
+            {
+                txtDL.DataBindings.Clear();
+                txtCL.DataBindings.Clear();
+                
+                cmbJudet.DataBindings.Clear();
+                txtSPI.DataBindings.Clear();
+                txtIDL.DataBindings.Clear();
+                PB.DataBindings.Clear();
+            }
+        }
+
+        private void protectiePanel(bool v)
+        {
+            txtDL.ReadOnly = v;
+            txtCL.ReadOnly = v;
+
+            txtSPI.ReadOnly = v;
+            cmbJudet.Enabled = !v;
+        }
+
+        private void golireCampuri()
+        {
+            txtDL.Text = "";
+            txtCL.Text = "";
+            txtIDL.Text = "";
+
+            txtSPI.Text = "";
+            cmbJudet.SelectedIndex = -1;
+            PB.ImageLocation = "";
+        }
+
+        private void btnRenuntare_Click(object sender, EventArgs e)
+        {
+            A3();
+        }
+
+        private void A3()
+        {
+            //Configurare comboBox-uri
+            cmbJudet.DropDownStyle = ComboBoxStyle.Simple;
+            //Modifcare lblOp
+            lblOp.Text = "";
+            //Çonfigurare(butoane)
+            configureazaButoane(true);
+            //Protectie componente Panel
+            protectiePanel(true);
+            //Legare controale
+            legareControale(true);
+        }
+
+        private void btnConfirmare_Click(object sender, EventArgs e)
+        {
+            A4();
+        }
+
+        private void A4()
+        {
+            if (lblOp.Text == "ADAUGARE")
+            {
+                if (!validareCampuriObligatorii()) return;
+                adauga_inregistrare();
+                golireCampuri();
+                //Pune cursor pe primul camp
+                txtDL.Focus();
+                refresh_grid(dataTable1BindingSource.Position);
+            }
+            else
+                MessageBox.Show("Operatie actualizare neefectuata");
+        }
+
+        private bool validareCampuriObligatorii()
+        {
+            //Validare de completare obligatorie campurile: Nume, CNP, Salariu, Localitate
+            if (txtDL.Text == "")
+            {
+                MessageBox.Show("Completati Denumirea !");
+                txtDL.Focus();
+                return false;
+            }
+            if (txtCL.Text == "")
+            {
+                MessageBox.Show("Completati Codul !");
+                txtCL.Focus();
+                return false;
+            }
+            //Validare Cod
+            if (verificaSemantic(txtCL.Text))
+            {
+                MessageBox.Show("Codul exista deja !");
+                txtCL.Focus();
+                return false;
+            }
+            if (cmbJudet.Text == "")
+            {
+                MessageBox.Show("Completati Judetul !");
+                cmbJudet.Focus();
+                return false;
+            }
+            if (txtSPI.Text == "")
+            {
+                MessageBox.Show("Alegeti Imaginea !");
+                cmbJudet.Focus();
+                return false;
+            }
+            
+            return true;
+        }
+
+        private void adauga_inregistrare()
+        {
+            string listaCampuri;
+            string listaValori;
+            OleDbConnection con = new OleDbConnection();
+            OleDbCommand cmd = new OleDbCommand();
+            con.ConnectionString = localitatiTableAdapter.Connection.ConnectionString;
+            cmd.Connection = con;
+            listaCampuri = "Dlocalitate, CodLocalitate, IdJudet,SpImagine";
+            listaValori = "'" + txtDL.Text + "'" +
+            ",'" + txtCL.Text + "'" +
+            "," + cmbJudet.SelectedValue +
+            ",'" + txtSPI.Text + "'";
+            cmd.CommandText = "Insert into Localitati(" + listaCampuri + ") " +
+            "Select " + listaValori;
+            MessageBox.Show(cmd.CommandText);
+            con.Open();
+            cmd.ExecuteNonQuery();
+            con.Close();
+        }
+
+        private void refresh_grid(int p)
+        {
+            dataTable1TableAdapter.Fill(dataSet1.DataTable1);
+            dataTable1BindingSource.Position = p;
+        }
+
+        private void txtCL_Leave(object sender, EventArgs e)
+        {
+            A5(txtCL);
+        }
+
+        private void A5(TextBox txtB)
+        {
+            decimal p;
+            if (txtCL.Text == "") return;
+            if (verificaSemantic(txtCL.Text))
+            {
+                MessageBox.Show("Codul exista deja !");
+                txtCL.Focus();
+                
+            }
+            try
+            {
+                p = Convert.ToDecimal(txtB.Text);
+            }
+            catch
+            {
+                MessageBox.Show("Format eronat");
+                txtB.Focus();
+            }
+
+
+        }
+
+        private void txtSPI_DoubleClick(object sender, EventArgs e)
+        {
+            A6();
+        }
+
+        private void A6()
+        {
+            if (lblOp.Text == "")
+                return;
+            if (openFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                txtSPI.Text = openFileDialog1.FileName;
+                PB.ImageLocation = txtSPI.Text;
+            }
+        }
+
+        private void cmbJudet_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            try
+            {
+                txtIDL.Text = cmbJudet.SelectedValue.ToString();
+            }catch{}
+        }
+
+        private Boolean verificaSemantic(string s) 
+        {
+           
+            string cldatabase;
+            foreach (DataRow r in dataSet1.DataTable1)
+            {
+                cldatabase = r["CodLocalitate"].ToString();
+
+                if (string.Equals(cldatabase, s))
+                    return true;
+            }
+            return false;
+        }
+    }
+}
